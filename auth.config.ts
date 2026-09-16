aff2b7bb6734d2250e976577981a91d3c511d31a
Apoorv Krishna <23je0139@iitism.ac.in>
2026-09-16 07:28:26 +0530

    Upgrade next-auth to 5.0.0-beta.32 to resolve OIDC iss CallbackRouteError bug

--- auth.config.ts
+++ auth.config.ts
@@ -13,7 +13,6 @@ export default {
         process.env.AUTH_GITHUB_SECRET ||
         process.env.GITHUB_SECRET ||
         process.env.GITHUB_CLIENT_SECRET,
-      checks: ["none"],
     }),
     Google({
       clientId:
